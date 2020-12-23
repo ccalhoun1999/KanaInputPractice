5e2dc94d6756f599e604318b0f68ebf96abbb427
Charles Calhoun <dodosssssssssssssss@gmail.com>
2020-12-22 18:41:03 -0800

    Working using console for checking

--- input.js
+++ input.js
@@ -10,22 +10,14 @@ function main() {
     var katakanaDaku;
     var katakanaYou;
 
+    var row1;
+    var row2;
+    var row3;
+    var row4;
+
     var testBank;
 
     gameStart();
-    
-    //build test bank
-    //start game by getting random element from test bank
-
-    
-
-
-
-
-
-
-
-
 
     function createBank() {
         var hiraganaValue = document.getElementById('hiraganaID');
@@ -34,6 +26,11 @@ function main() {
         var katakanaValue = document.getElementById('katakanaID');
         var katakanaDakuValue = document.getElementById('katakanaDakuID');
         var katakanaYouValue = document.getElementById('katakanaYouID');
+
+        var row1Value = document.getElementById('row1ID')
+        var row2Value = document.getElementById('row2ID')
+        var row3Value = document.getElementById('row3ID')
+        var row4Value = document.getElementById('row4ID')
 
         var bank = [];
         
@@ -55,7 +52,20 @@ function main() {
         if(katakanaYouValue.checked) {
             bank = bank.concat(katakanaYou);
         }
-
+        if(!hiraganaValue.checked){
+            if(row1Value.checked){
+                bank = bank.concat(row1);
+            }
+            if(row2Value.checked){
+                bank = bank.concat(row2);
+            }
+            if(row3Value.checked){
+                bank = bank.concat(row3);
+            }
+            if(row4Value.checked){
+                bank = bank.concat(row4);
+            }
+        }
         return bank;
     }
 
@@ -64,11 +74,14 @@ function main() {
         userInputElement = document.getElementById('userInputID');
         userInputElement.onkeydown = function(key) {
             if(key.code == "Enter") {
-                userEnterPress();
+                if(userInputElement.value != ""){
+                    userEnterPress();
+                }
             }
         }
         document.getElementById("updateBankID").onclick = function() {
             testBank = createBank();
+            newKana();
         }
     }
 
@@ -76,14 +89,28 @@ function main() {
         linkHTML();
         setupKanaVariables();
         testBank = createBank();
+        newKana();
     }
 
     function userEnterPress() {
-        console.log("userEnterPress()")
+        input = userInputElement.value;
+        answer = kanaTestElement.innerHTML;
+        if(input.slice(-1) == answer){
+            console.log("Correct");
+        } else {
+            console.log("Wrong");
+        }
+        userInputElement.value = "";
+        newKana();
     }
 
     function newKana() {
-        
+        var nk = testBank[Math.floor(Math.random() * testBank.length)];
+        while(nk == kanaTestElement.innerHTML){ //reroll until a new kana comes up
+            nk = testBank[Math.floor(Math.random() * testBank.length)];
+        }
+
+        kanaTestElement.innerHTML = nk;
     }
 
     function setupKanaVariables() {
@@ -97,13 +124,13 @@ function main() {
                         "や","ゆ","よ",
                         "ら","り","る","れ","ろ",
                         "わ","を","ん"
-                        ]
+                        ];
         hiraganaDaku    = ["が","ぎ","ぐ","げ","ご",
                         "ざ","じ","ず","ぜ","ぞ",
                         "だ","ぢ","づ","で","ど",
                         "ば","び","ぶ","べ","ぼ",
                         "ぱ","ぴ","ぷ","ぺ","ぽ"
-                        ]
+                        ];
         hiraganaYou     = ["きゃ","きゅ","きょ",
                         "ぎゃ","ぎゅ","ぎょ",
                         "しゃ","しゅ","しょ",
@@ -115,7 +142,7 @@ function main() {
                         "ぴゃ","ぴゅ","ぴょ",
                         "みゃ","みゅ","みょ",
                         "りゃ","りゅ","りょ"
-                        ]
+                        ];
         katakana        =  ["ア","イ","ウ","エ","オ",
                         "カ","キ","ク","ケ","コ",
                         "サ","シ","ス","セ","ソ",
@@ -126,13 +153,13 @@ function main() {
                         "ヤ","ユ","ヨ",
                         "ラ","リ","ル","レ","ロ",
                         "ワ","ヲ","ン"
-                        ]
+                        ];
         katakanaDaku    = ["ガ","ギ","グ","ゲ","ゴ",
                         "ザ","ジ","ズ","ゼ","ゾ",
                         "ダ","ヂ","ヅ","デ","ド",
                         "バ","ビ","ブ","ベ","ボ",
                         "パ","ピ","プ","ペ","ポ"
-                        ]
+                        ];
         katakanaYou     = ["キャ","キュ","キョ",
                         "ギャ","ギュ","ギョ",
                         "シャ","シュ","ショ",
@@ -144,6 +171,10 @@ function main() {
                         "ピャ","ピュ","ピョ",
                         "ミャ","ミュ","ミョ",
                         "リャ","リュ","リョ"
-                        ]
+                        ];
+        row1            = ["ぬ","ふ","あ","う","え","お","や","ゆ","よ","わ","ほ","へ"];
+        row2            = ["た","て","い","す","か","ん","な","に","ら","せ","む"];
+        row3            = ["ち","と","し","は","き","く","ま","の","り","れ","け"];
+        row4            = ["つ","さ","そ","ひ","こ","み","も","ね","る","め","ろ"];
     }
 }
